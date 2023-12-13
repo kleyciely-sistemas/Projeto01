86160696fff2b96e46d131c30c2a5ecf3af8c0e2
kleyci <kleycielya@gmail.com>
2023-12-13 20:48:34 -0300

    teste1

--- src/fundamentos/AreaCircunferencia.java
+++ src/fundamentos/AreaCircunferencia.java
@@ -2,6 +2,15 @@ package fundamentos;
 
 public class AreaCircunferencia {
     public static void main(String[] args) {
-        System.out.println("tela de teste");
+        double raio = 3.4;
+        final double PI = 3.14159;
+
+        double area = PI * raio * raio;
+
+        System.out.println(area);
+
+        raio = 10;
+        area = PI * raio * raio;
+        System.out.println("Área = " + area + "m2.");
     }
 }
---
+++ src/fundamentos/Temperatura.java
@@ -0,0 +1,8 @@
+package fundamentos;
+
+public class Temperatura {
+    public static void main(String[] args) {
+        // (°F - 32) * 5/9 = °C
+
+    }
+}
